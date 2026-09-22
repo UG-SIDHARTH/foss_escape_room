8743ec49a39be42a0a0d5ce311d3290bb4d226af
UG-SIDHARTH <appuvichu10@gmail.com>
2026-09-22 18:44:35 +0530

    fix(client): disable extreme javascript obfuscation which causes blank screens on some devices

--- client/vite.config.js
+++ client/vite.config.js
@@ -14,41 +14,7 @@ export default defineConfig(({ command, mode }) => {
   return {
     plugins: [
       basicSsl(),
-      react(),
-      mode === 'production' && vitePluginJavascriptObfuscator({
-        include: ['src/**/*.js', 'src/**/*.jsx'],
-        exclude: [/node_modules/],
-        apply: 'build',
-        debugger: true,
-        options: {
-          compact: true,
-          controlFlowFlattening: true,
-          controlFlowFlatteningThreshold: 1,
-          deadCodeInjection: true,
-          deadCodeInjectionThreshold: 0.4,
-          debugProtection: false,
-          disableConsoleOutput: false,
-          identifierNamesGenerator: 'hexadecimal',
-          log: false,
-          numbersToExpressions: true,
-          renameGlobals: false,
-          selfDefending: true,
-          simplify: true,
-          splitStrings: true,
-          splitStringsChunkLength: 10,
-          stringArray: true,
-          stringArrayEncoding: ['base64'],
-          stringArrayIndexShift: true,
-          stringArrayRotate: true,
-          stringArrayShuffle: true,
-          stringArrayWrappersCount: 1,
-          stringArrayWrappersChainedCalls: true,
-          stringArrayWrappersParametersMaxCount: 2,
-          stringArrayWrappersType: 'variable',
-          stringArrayThreshold: 0.75,
-          unicodeEscapeSequence: false
-        }
-      })
+      react()
     ],
     server: {
       port: 6000,
