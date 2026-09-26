f1ee984a0a276fabe81767bae9b2b4cb07a5a448
UG-SIDHARTH <appuvichu10@gmail.com>
2026-09-26 11:22:47 +0530

    feat: add SQLite database initialization module and update Docker volume mount

--- server/db.js
+++ server/db.js
@@ -1,7 +1,13 @@
 const Database = require('better-sqlite3');
 const path = require('path');
+const fs = require('fs');
 
-const dbPath = path.join(__dirname, 'escape_room.db');
+const dataDir = path.join(__dirname, 'data');
+if (!fs.existsSync(dataDir)) {
+  fs.mkdirSync(dataDir, { recursive: true });
+}
+
+const dbPath = path.join(dataDir, 'escape_room.db');
 const db = new Database(dbPath, { verbose: console.log });
 
 function initDb() {
